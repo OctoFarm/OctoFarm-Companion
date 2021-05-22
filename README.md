63c947c293a9dc81b897912514e95182d5106273
David Zwart <davidzwa@gmail.com>
2021-05-22 14:53:50 +0200

    Further customise plugin and add readme

--- README.md
+++ README.md
@@ -1,17 +1,42 @@
 # OctoFarm-Companion
 
-**TODO:** Describe what your plugin does.
+This plugin is your friend when using OctoFarm with one or more OctoPrint instances. The plugin is intended to simplify
+the initial connection with OctoFarm. Next to that some more functionality will be built in future.
+
+Current feature(s):
+- Auto-registration - send your OctoPrint connection parameters to OctoFarm safely, to make setting up printers a breeze.
+
+Future features:
+- Filament Pedometer - send filament usage data to OctoFarm, making the filament manager plugin and its PostGres database unnecessary.
+- Http Tunnel - connect to OctoFarm, to make connection to printers a breeze especially over docker, VPN, DMZ, VLAN, the cloud or other complex network setups.
+- Single-sign-On - client-to-machine (C2M) and machine-to-machine (M2M) authentication removing the need for more than 1 set of credentials across the farm.
+
+For more feature requests, bugs, or ideas please head over to https://github.com/OctoFarm/OctoFarm/discussions.
 
 ## Setup
+**!! Work in progress !!**
 
 Install via the bundled [Plugin Manager](https://docs.octoprint.org/en/master/bundledplugins/pluginmanager.html)
 or manually using this URL:
 
     https://github.com/OctoFarm/OctoFarm-Companion/archive/master.zip
 
-**TODO:** Describe how to install your plugin, if more needs to be done than just installing it via pip or through
-the plugin manager.
+Please configure the plugin completely for one or more printers before checking OctoFarm.
 
 ## Configuration
+**Warning - restoring from a OctoPrint backup can be cause of security weakness due to copied passwords. In the end we are not responsible for your choices, but we advise to change the `accessControl:salt` and to regenerate the main user account for each OctoPrint instance.**
 
-**TODO:** Describe your plugin's configuration options (if any).
+### Configuration - auto-registration
+Configuring the auto-registration properly can massively improve the steps you need to undertake to setup your farm.
+- `octofarmHost` the host to reach OctoFarm with (IP, localhost, domain name, etc)
+- `octofarmPort` the port to approach the OctoFarm server (number)
+
+We understand if you restore OctoPrint backups to install new OctoPrints. For that reason we've introduced two unique ID's (UUID).
+- `persistenceUuid` a unique identifier stored in the plugin folder in `device.json`, which is excluded from backups to prevent duplicate printers.
+- `deviceUuid` a unique identifier stored in the `config.yaml` at startup.
+
+Periodic updates
+- `ping` the time in seconds between each call to OctoFarm (default is 15 * 60, or 15 minutes)
+
+The plugin will use `server:host` and `server:port` to give OctoFarm a handle to connect back to this OctoPrint. This is often incorrect, if your OctoPrint is behind a proxy, in a VM, UnRaid, a different device, DMZ, in a docker container or in a VPN.
+At this moment this needs to be rectified in OctoFarm. Later we will allow more advanced ways to fix this, but for now we believe this plugin is going to make it much easier already.
--- octofarm_companion/__init__.py
+++ octofarm_companion/__init__.py
@@ -10,124 +10,145 @@ import octoprint.plugin
 import requests
 from octoprint.util import RepeatedTimer
 
+def is_docker():
+    path = '/proc/self/cgroup'
+    return (
+        os.path.exists('/.dockerenv') or
+        os.path.isfile(path) and any('docker' in line for line in open(path))
+    )
 
 class OctoFarmCompanionPlugin(
-	octoprint.plugin.StartupPlugin,
-	octoprint.plugin.ShutdownPlugin,
-	octoprint.plugin.BlueprintPlugin,
-	octoprint.plugin.SettingsPlugin,
-	octoprint.plugin.OctoPrintPlugin,
-	octoprint.plugin.AssetPlugin,
-	octoprint.plugin.TemplatePlugin
+    octoprint.plugin.StartupPlugin,
+    octoprint.plugin.ShutdownPlugin,
+    octoprint.plugin.BlueprintPlugin,
+    octoprint.plugin.SettingsPlugin,
+    octoprint.plugin.OctoPrintPlugin,
+    # octoprint.plugin.AssetPlugin,
+    # octoprint.plugin.TemplatePlugin
 ):
-	def __init__(self):
-		self._ping_worker = None
-		self._excluded_file = "device.json"
+    def __init__(self):
+        self._ping_worker = None
+        self._excluded_file = "device.json"
 
-	def on_after_startup(self):
-		self._start_periodic_check()
+    def on_after_startup(self):
+        self._start_periodic_check()
 
-	def get_settings_defaults(self):
-		return {
-			"publicHost": "http://localhost",
-			"publicPort": 4000,
-			"ping": 15
-		}
+    def get_settings_defaults(self):
+        return {
+            "octofarmHost": "http://localhost",
+            "octofarmPort": 4000,
+			"deviceUuid": None,
+            "ping": 15 * 60
+        }
 
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/octofarm_companion.js"],
-			css=["css/octofarm_companion.css"],
-			less=["less/octofarm_companion.less"]
-		)
+    def get_assets(self):
+        # Define your plugin's asset files to automatically include in the
+        # core UI here.
+        return dict(
+            js=["js/octofarm_companion.js"],
+            css=["css/octofarm_companion.css"],
+            less=["less/octofarm_companion.less"]
+        )
 
-	def initialize(self):
-		self._get_persistence_uuid()
+    def initialize(self):
+        self._get_persistence_uuid()
 
-	def _get_persistence_uuid(self):
-		filepath = os.path.join(self.get_plugin_data_folder(), self._excluded_file)
+    def _get_persistence_uuid(self):
+        filepath = os.path.join(self.get_plugin_data_folder(), self._excluded_file)
 
-		if os.path.exists(filepath):
-			try:
-				with io.open(filepath, "r", encoding="utf-8") as f:
-					device_uuid_file = f.read()
-					device_uuid_json = json.loads(device_uuid_file)
-					return device_uuid_json["device_uuid"]
-			except json.decoder.JSONDecodeError as e:
-				self._logger.warning("OctoFarm persisted device Id file was of invalid format.")
-				return self._write_new_device_uuid(filepath)
-		else:
-			return self._write_new_device_uuid(filepath)
+        if os.path.exists(filepath):
+            try:
+                with io.open(filepath, "r", encoding="utf-8") as f:
+                    device_uuid_file = f.read()
+                    device_uuid_json = json.loads(device_uuid_file)
+                    return device_uuid_json["device_uuid"]
+            except json.decoder.JSONDecodeError as e:
+                self._logger.warning(
+                    "OctoFarm persisted device Id file was of invalid format.")
+                return self._write_new_device_uuid(filepath)
+        else:
+            return self._write_new_device_uuid(filepath)
 
-	def _write_new_device_uuid(self, filepath):
-		persistedDeviceUuid = str(uuid.uuid4())
-		json_data = {'device_uuid': persistedDeviceUuid}
+    def _write_new_device_uuid(self, filepath):
+        persistedDeviceUuid = str(uuid.uuid4())
+        json_data = {'device_uuid': persistedDeviceUuid}
 
-		with io.open(filepath, "w", encoding="utf-8") as f:
-			f.write(json.dumps(json_data))
+        with io.open(filepath, "w", encoding="utf-8") as f:
+            f.write(json.dumps(json_data))
 
-		self._logger.info("OctoFarm persisted device Id file was stored.")
+        self._logger.info("OctoFarm persisted device Id file was stored.")
 
-		return persistedDeviceUuid
+        return persistedDeviceUuid
 
-	def _get_uuid(self):
-		deviceUuid = self._settings.get(["deviceUuid"])
-		if deviceUuid is None:
-			deviceUuid = str(uuid.uuid4())
-			self._settings.set(["deviceUuid"], deviceUuid)
-			self._settings.save()
-		return deviceUuid
+    def _get_uuid(self):
+        deviceUuid = self._settings.get(["deviceUuid"])
+        if deviceUuid is None:
+            deviceUuid = str(uuid.uuid4())
+            self._settings.set(["deviceUuid"], deviceUuid)
+            self._settings.save()
+        return deviceUuid
 
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-		# for details.
-		return dict(
-			octofarm_companion=dict(
-				displayName="Octofarm-Companion Plugin",
-				displayVersion=self._plugin_version,
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
+        # for details.
+        return dict(
+            octofarm_companion=dict(
+                displayName="Octofarm-Companion Plugin",
+                displayVersion=self._plugin_version,
 
-				# version check: github repository
-				type="github_release",
-				user="octofarm",
-				repo="OctoFarm-Companion",
-				current=self._plugin_version,
+                # version check: github repository
+                type="github_release",
+                user="octofarm",
+                repo="OctoFarm-Companion",
+                current=self._plugin_version,
 
-				# update method: pip
-				pip="https://github.com/octofarm/OctoFarm-Companion/archive/{target_version}.zip"
-			)
-		)
+                # update method: pip
+                pip="https://github.com/octofarm/OctoFarm-Companion/archive/{target_version}.zip"
+            )
+        )
 
-	def _start_periodic_check(self):
-		if self._ping_worker is None:
-			ping_interval = self._settings.get_int(["ping"])
-			if ping_interval:
-				self._ping_worker = RepeatedTimer(
-					ping_interval, self._check_octofarm, run_first=True
-				)
-				self._ping_worker.start()
+    def _start_periodic_check(self):
+        if self._ping_worker is None:
+            ping_interval = self._settings.get_int(["ping"])
+            if ping_interval:
+                self._ping_worker = RepeatedTimer(
+                    ping_interval, self._check_octofarm, run_first=True
+                )
+                self._ping_worker.start()
 
-	def _check_octofarm(self):
-		octofarm_host = self._settings.get(["publicHost"])
-		octofarm_port = self._settings.get(["publicPort"])
+    def _check_octofarm(self):
+        octofarm_host = self._settings.get(["octofarmHost"])
+        octofarm_port = self._settings.get(["octofarmPort"])
 
-		if octofarm_host is not None and octofarm_port is not None:
-			persisted_device_uuid = self._get_persistence_uuid()
-			deviceUuid = self._get_uuid()
+        host = self._settings.global_get(["server", "host"])
+        port = self._settings.global_get(["server", "port"])
+        allow_cross_origin = self._settings.global_get(["api", "allowCrossOrigin"])
 
-			check_data = {"deviceUuid": deviceUuid,
-						  "persistenceUuid": persisted_device_uuid}
-			self._logger.info("Checking OctoFarm server")
+        if octofarm_host is not None and octofarm_port is not None:
+            try:
+                persisted_device_uuid = self._get_persistence_uuid()
+                device_uuid = self._get_uuid()
 
-			response = requests.get(f"{octofarm_host}:{octofarm_port}/plugins/check", data=check_data)
-			self._logger.info(response.text)
+                check_data = {
+                    "deviceUuid": device_uuid,
+                    "persistenceUuid": persisted_device_uuid,
+                    "host": host,
+                    "port": int(port),
+                    "docker": bool(is_docker()),
+                    "allowCrossOrigin": bool(allow_cross_origin)
+                }
+                self._logger.info("Checking OctoFarm server")
 
-			self._logger.info("Done checking OctoFarm server")
+                response = requests.get(f"{octofarm_host}:{octofarm_port}/plugins/check", json=check_data)
+                self._logger.info(response.text)
 
-	def additional_excludes_hook(self, excludes, *args, **kwargs):
-		return [self._excluded_file]
+                self._logger.debug("Done checking OctoFarm server")
+            except requests.exceptions.ConnectionError:
+                self._logger.error("Error connecting to OctoFarm")
+
+    def additional_excludes_hook(self, excludes, *args, **kwargs):
+        return [self._excluded_file]
 
 
 __plugin_name__ = "OctoFarm Companion"
@@ -137,11 +158,11 @@ __plugin_pythoncompat__ = ">=3,<4"
 
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = OctoFarmCompanionPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = OctoFarmCompanionPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
-		"octoprint.plugin.backup.additional_excludes": __plugin_implementation__.additional_excludes_hook
-	}
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+        "octoprint.plugin.backup.additional_excludes": __plugin_implementation__.additional_excludes_hook
+    }
